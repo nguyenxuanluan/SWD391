f33bbc7432c3cbe4d9c447d777d12305c4037d06
LuanNX <Luannxse04649@fpt.edu.vn>
2018-07-12 16:53:50 +0700

    First

---
+++ src/main/java/com/fpt/edu/swd391/Swd391Application.java
@@ -0,0 +1,12 @@
+package com.fpt.edu.swd391;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swd391Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swd391Application.class, args);
+	}
+}
---
+++ src/test/java/com/fpt/edu/swd391/Swd391ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fpt.edu.swd391;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Swd391ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
